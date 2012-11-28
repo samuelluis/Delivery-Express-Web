3e793180a8a086bdfeae3bb06acd7f91e6130a7a
Samuel Luis De La Cruz <sammultiservices@gmail.com>
2012-11-27 20:50:47 -0400

    Repository Initialization of Project With Basic Structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deliveryexpressweb::Application.config.secret_token = 'c4296422deabb89b1a5f8f4f6726b58fa118e326bcfbda5adef5a4ade238787394b5b612077bf0aeb6b5c4bb506bc6bfe4af69072826dddb22699a3313722307'
